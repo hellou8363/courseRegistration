2bdf6f8eae84ea03cdbe23dc8e9547432599f9fd
eunhye han <hellou8363@gmail.com>
2024-05-08 19:55:43 +0900

    🎉 프로젝트 시작

---
+++ src/main/kotlin/org/zerock/courseregistration/CourseRegistrationApplication.kt
@@ -0,0 +1,11 @@
+package org.zerock.courseregistration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourseRegistrationApplication
+
+fun main(args: Array<String>) {
+    runApplication<CourseRegistrationApplication>(*args)
+}
