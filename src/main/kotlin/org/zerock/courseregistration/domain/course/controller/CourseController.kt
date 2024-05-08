71f33b8de6de07f800a705e7fa23cdf533af90e0
eunhye han <hellou8363@gmail.com>
2024-05-08 20:54:02 +0900

    ✨ course controller내 메서드 구현 추가

--- src/main/kotlin/org/zerock/courseregistration/domain/course/controller/CourseController.kt
+++ src/main/kotlin/org/zerock/courseregistration/domain/course/controller/CourseController.kt
@@ -1,28 +1,32 @@
 package org.zerock.courseregistration.domain.course.controller
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.zerock.courseregistration.domain.course.dto.CourseResponse
 import org.zerock.courseregistration.domain.course.dto.CreateCourseRequest
 import org.zerock.courseregistration.domain.course.dto.UpdateCourseRequest
+import org.zerock.courseregistration.domain.course.service.CourseService
 
 @RequestMapping("/courses")
 @RestController
-class CourseController {
+class CourseController(
+    private val courseService: CourseService
+) {
 
     @GetMapping()
-    fun getCourses(): ResponseEntity<List<CourseResponse>> {
-        TODO()
+    fun getCourseList(): ResponseEntity<List<CourseResponse>> {
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.getAllCourseList())
     }
 
     @GetMapping("/{courseId}")
     fun getCourse(@PathVariable courseId: Long): ResponseEntity<CourseResponse> {
-        TODO()
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.getCourseById(courseId))
     }
 
     @PostMapping
     fun createCourse(@RequestBody createCourseRequest: CreateCourseRequest): ResponseEntity<CourseResponse> {
-        TODO()
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.createCourse(createCourseRequest))
     }
 
     @PutMapping("/{courseId}")
@@ -30,11 +34,11 @@ class CourseController {
         @PathVariable courseId: Long,
         @RequestBody updateCourseRequest: UpdateCourseRequest
     ): ResponseEntity<CourseResponse> {
-        TODO()
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.updateCourse(courseId, updateCourseRequest))
     }
 
     @DeleteMapping("/{courseId}")
     fun deleteCourse(@PathVariable courseId: Long): ResponseEntity<Unit> {
-        TODO()
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 }
