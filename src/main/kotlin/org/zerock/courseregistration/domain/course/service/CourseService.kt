d2fd554e71b7d0bee10bd2253146c00a77cad5dd
eunhye han <hellou8363@gmail.com>
2024-05-09 08:08:54 +0900

    ✨ lecture, courseApplication의 service를 course service 내 포함하여 구현

--- src/main/kotlin/org/zerock/courseregistration/domain/course/service/CourseService.kt
+++ src/main/kotlin/org/zerock/courseregistration/domain/course/service/CourseService.kt
@@ -3,6 +3,12 @@ package org.zerock.courseregistration.domain.course.service
 import org.zerock.courseregistration.domain.course.dto.CourseResponse
 import org.zerock.courseregistration.domain.course.dto.CreateCourseRequest
 import org.zerock.courseregistration.domain.course.dto.UpdateCourseRequest
+import org.zerock.courseregistration.domain.courseapplication.dto.ApplyCourseRequest
+import org.zerock.courseregistration.domain.courseapplication.dto.CourseApplicationResponse
+import org.zerock.courseregistration.domain.courseapplication.dto.UpdateApplicationStatusRequest
+import org.zerock.courseregistration.domain.lecture.dto.AddLectureRequest
+import org.zerock.courseregistration.domain.lecture.dto.LectureResponse
+import org.zerock.courseregistration.domain.lecture.dto.UpdateLectureRequest
 
 interface CourseService {
 
@@ -15,4 +21,33 @@ interface CourseService {
     fun updateCourse(courseId: Long, request: UpdateCourseRequest): CourseResponse
 
     fun deleteCourse(courseId: Long)
+
+    fun getLectureList(courseId: Long): List<LectureResponse>
+
+    fun getLecture(courseId: Long, lectureId: Long): LectureResponse
+
+    fun addLecture(courseId: Long, request: AddLectureRequest): LectureResponse
+
+    fun updateLecture(
+        courseId: Long,
+        lectureId: Long,
+        request: UpdateLectureRequest
+    ): LectureResponse
+
+    fun removeLecture(courseId: Long, lectureId: Long)
+
+    fun applyCourse(courseId: Long, request: ApplyCourseRequest): CourseApplicationResponse
+
+    fun getCourseApplication(
+        courseId: Long,
+        applicationId: Long
+    ): CourseApplicationResponse
+
+    fun getCourseApplicationList(courseId: Long): List<CourseApplicationResponse>
+
+    fun updateCourseApplicationStatus(
+        courseId: Long,
+        applicationId: Long,
+        request: UpdateApplicationStatusRequest
+    ): CourseApplicationResponse
 }
