41393284dacffa498a0c6de4f972d380841278cc
eunhye han <hellou8363@gmail.com>
2024-05-09 08:53:05 +0900

    ✨ user controller 내 todo -> 구현 코드로 추가

--- src/main/kotlin/org/zerock/courseregistration/domain/user/controller/UserController.kt
+++ src/main/kotlin/org/zerock/courseregistration/domain/user/controller/UserController.kt
@@ -1,17 +1,21 @@
 package org.zerock.courseregistration.domain.user.controller
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.zerock.courseregistration.domain.user.dto.SignUpRequest
 import org.zerock.courseregistration.domain.user.dto.UpdateUserProfileRequest
 import org.zerock.courseregistration.domain.user.dto.UserResponse
+import org.zerock.courseregistration.domain.user.service.UserService
 
 @RestController
-class UserController {
+class UserController(
+    private val userService: UserService
+) {
 
     @PostMapping("/signup")
     fun signup(@RequestBody signUpRequest: SignUpRequest): ResponseEntity<UserResponse> {
-        TODO()
+        return ResponseEntity.status(HttpStatus.OK).body(userService.signUp(signUpRequest))
     }
 
     @PutMapping("/users/{userId}/profile")
@@ -19,6 +23,7 @@ class UserController {
         @PathVariable userId: Long,
         @RequestBody updateUserProfileRequest: UpdateUserProfileRequest
     ): ResponseEntity<UserResponse> {
-        TODO()
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(userService.updateUserProfile(userId, updateUserProfileRequest))
     }
 }
